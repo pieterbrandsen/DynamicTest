f0c8cb90d7d5064036c7859312510d9ec4bcda9e
Pieter <pieterbrandsen2@gmail.com>
2021-06-29 22:31:29 +0200

    Removed old code

--- DynamicTest/Server/Controllers/HomeController.cs
+++ DynamicTest/Server/Controllers/HomeController.cs
@@ -20,38 +20,6 @@ namespace DynamicTest.Server.Controllers
             string json = r.ReadToEnd();
             var jsonObj = JsonConvert.DeserializeObject<object>(json);
             Json.ConvertJsonStringToObject(json);
-            //try
-            //{
-            //    var jsonDictionary = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
-            //    var dictionary = new Dictionary<string, Dictionary<string, string>>();
-
-            //    foreach (var itemGroup in jsonDictionary)
-            //    {
-            //        var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(itemGroup.Value.ToString());
-            //        dictionary.Add(itemGroup.Key, items);
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //}
-
-            //try
-            //{
-            //    var jsonArray = JsonConvert.DeserializeObject<dynamic[]>(json);
-            //    var dictionary = new List<Dictionary<string, string>>();
-
-            //    foreach (var itemGroup in jsonArray)
-            //    {
-            //        var a = itemGroup.ToString();
-            //        var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(a);
-            //        dictionary.Add(items);
-            //    }
-            //}
-            //catch (Exception)
-            //{
-
-            //}
-
 
             return null;
         }
