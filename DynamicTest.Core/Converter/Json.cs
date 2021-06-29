a799940b5ecd6674b05178191f0087dee526b50c
Pieter <pieterbrandsen2@gmail.com>
2021-06-29 20:37:09 +0200

    Generic List or Dictionary generator

---
+++ DynamicTest.Core/Converter/Json.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Json;
+
+namespace DynamicTest.Core.Converter
+{
+    public static class Json
+    {
+        private static List<object> RecursiveArrayConverter(string json)
+        {
+            var jsonObj = JsonConvert.DeserializeObject<List<dynamic>>(json);
+            var list = new List<object>();
+            foreach (var item in jsonObj)
+            {
+                var itemString = item.ToString();
+                if (!StringIsValidJson(itemString)) list.Add(item.ToString());
+                else if (StringIsJsonObject(itemString)) list.Add(RecursiveObjectConverter(item.ToString()));
+                else list.Add(RecursiveArrayConverter(item.ToString()));
+            }
+            return list;
+        }
+        private static Dictionary<string, object> RecursiveObjectConverter(string json)
+        {
+            var jsonObj = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            var dictionary = new Dictionary<string, object>();
+            foreach (var item in jsonObj)
+            {
+                var itemString = item.Value.ToString();
+                if (!StringIsValidJson(itemString)) dictionary.Add(item.Key, itemString);
+                else if (StringIsJsonObject(itemString)) dictionary.Add(item.Key, RecursiveObjectConverter(itemString));
+                else dictionary.Add(item.Key, RecursiveArrayConverter(itemString));
+            }
+            return dictionary;
+        }
+        private static bool StringIsJsonObject(string jsonString)
+        {
+            try
+            {
+                JsonConvert.DeserializeObject<Dictionary<string, object>>(jsonString);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private static bool StringIsValidJson(string jsonString)
+        {
+            try
+            {
+                if ((jsonString.StartsWith("{") && jsonString.EndsWith("}")) || //For object
+    (jsonString.StartsWith("[") && jsonString.EndsWith("]"))) //For array
+                {
+                    JsonValue.Parse(jsonString);
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static object ConvertJsonStringToObject(string jsonString)
+        {
+            var obj = new object();
+            if (StringIsJsonObject(jsonString))
+            {
+                obj = RecursiveObjectConverter(jsonString);
+            }
+            else
+            {
+                obj = RecursiveArrayConverter(jsonString);
+            }
+
+            return obj;
+        }
+    }
+}
--- DynamicTest/Server/Controllers/HomeController.cs
+++ DynamicTest/Server/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using DynamicTest.Core.Converter;
 
 namespace DynamicTest.Server.Controllers
 {
@@ -17,37 +18,39 @@ namespace DynamicTest.Server.Controllers
         {
             using StreamReader r = new StreamReader("test.json");
             string json = r.ReadToEnd();
-            try
-            {
-                var jsonDictionary = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
-                var dictionary = new Dictionary<string, Dictionary<string, string>>();
+            var jsonObj = JsonConvert.DeserializeObject<object>(json);
+            Json.ConvertJsonStringToObject(json);
+            //try
+            //{
+            //    var jsonDictionary = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(json);
+            //    var dictionary = new Dictionary<string, Dictionary<string, string>>();
 
-                foreach (var itemGroup in jsonDictionary)
-                {
-                    var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(itemGroup.Value.ToString());
-                    dictionary.Add(itemGroup.Key, items);
-                }
-            }
-            catch (Exception)
-            {
-            }
+            //    foreach (var itemGroup in jsonDictionary)
+            //    {
+            //        var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(itemGroup.Value.ToString());
+            //        dictionary.Add(itemGroup.Key, items);
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //}
 
-            try
-            {
-                var jsonArray = JsonConvert.DeserializeObject<dynamic[]>(json);
-                var dictionary = new List<Dictionary<string, string>>();
+            //try
+            //{
+            //    var jsonArray = JsonConvert.DeserializeObject<dynamic[]>(json);
+            //    var dictionary = new List<Dictionary<string, string>>();
 
-                foreach (var itemGroup in jsonArray)
-                {
-                    var a = itemGroup.ToString();
-                    var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(a);
-                    dictionary.Add(items);
-                }
-            }
-            catch (Exception)
-            {
+            //    foreach (var itemGroup in jsonArray)
+            //    {
+            //        var a = itemGroup.ToString();
+            //        var items = JsonConvert.DeserializeObject<Dictionary<string, string>>(a);
+            //        dictionary.Add(items);
+            //    }
+            //}
+            //catch (Exception)
+            //{
 
-            }
+            //}
 
 
             return null;
