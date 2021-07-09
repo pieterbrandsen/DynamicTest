496d4a991e0370139c36ee726de5b8bf541b0f8c
Pieter <pieterbrandsen2@gmail.com>
2021-07-09 22:05:51 +0200

    Added first generic table concept that can display one iteration of an dynamic dictionary

---
+++ DynamicTest.Core/Models/DynamicDictionary.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace DynamicTest.Core.Models
+{
+    public class DynamicDictionary : System.Dynamic.DynamicObject
+    {
+        Dictionary<string, object> dictionary = new Dictionary<string, object>();
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            string name = binder.Name;
+            return dictionary.TryGetValue(name, out result);
+        }
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            dictionary[binder.Name] = value;
+            return true;
+        }
+        //The GetDynamicMemberNames method of DynamicObject class must be overridden and return the property names to perform data operation and editing while using DynamicObject.
+        public override IEnumerable<string> GetDynamicMemberNames()
+        {
+            return dictionary?.Keys;
+        }
+    }
+}
